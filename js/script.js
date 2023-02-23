a7f2618f8312339fd57aaf65c3545a624cd359e3
TomasBautistaVillalba <tomasbautistavillalba@outlook.es>
2023-02-23 19:05:44 -0300

    proyectoFinal-js

--- js/script.js
+++ js/script.js
@@ -1,185 +1,11 @@
-let stockProductos = [
-                    // MATES 
-    {
-        id: "Mate imperial liso",
-        titulo: "Mate imperial liso",
-        imagen: "./fotos/mates/mate-imperial-liso.jpeg",
-        categoria: {
-            nombre: "mates",
-            id: "mates",
-        },
-        precio: 7000,
-    },
-    {
-        id: "Mate imperial cincelado",
-        titulo: "Mate imperial cincelado",
-        imagen: "./fotos/mates/mateimperialcincelado.jpeg",
-        categoria: {
-            nombre: "mates",
-            id: "mates",
-        },
-        precio: 9000,
-    },
-    {
-        id: "Mate imperial premium",
-        titulo: "Mate imperial premium",
-        imagen: "./fotos/mates/mateimperialpremium.png",
-        categoria: {
-            nombre: "mates",
-            id: "mates",
-        },
-        precio: 12000,
-    },
-    {
-        id: "Mate camionero cincelado",
-        titulo: "Mate camionero cincelado",
-        imagen: "./fotos/mates/mate-camionero-cincelado.jpeg",
-        categoria: {
-            nombre: "mates",
-            id: "mates",
-        },
-        precio: 7500,
-    },
-    {
-        id: "Mate camionero liso",
-        titulo: "Mate camionero liso",
-        imagen: "./fotos/mates/mate-camionero-liso.jpeg",
-        categoria: {
-            nombre: "mates",
-            id: "mates",
-        },
-        precio: 6000,
-    },
-    {
-        id: "Mate madera calden",
-        titulo: "Mate madera calden",
-        imagen: "./fotos/mates/mate-madera-calden.jpeg",
-        categoria: {
-            nombre: "mates",
-            id: "mates",
-        },
-        precio: 5000,
-    },
+let stockProductos = [];
 
-                    // BOMBILLAS
-
-    {
-        id: "Pico loro alpaca cincelada",
-        titulo: "Pico loro alpaca cincelada",
-        imagen: "./fotos/bombillas/picoloroalpaca.jpeg",
-        categoria: {
-            nombre: "bombillas",
-            id: "bombillas",
-        },
-        precio: 3800,
-    },
-    {
-        id: "Pico loro lisa",
-        titulo: "Pico loro lisa",
-        imagen: "./fotos/bombillas/picolorolisa.jpg",
-        categoria: {
-            nombre: "bombillas",
-            id: "bombillas",
-        },
-        precio: 3200,
-    },
-    {
-        id: "Bombillon pico bronce cincelada",
-        titulo: "Bombillon pico bronce cincelada",
-        imagen: "./fotos/bombillas/bombillonpicobronce.jpg",
-        categoria: {
-            nombre: "bombillas",
-            id: "bombillas",
-        },
-        precio: 4500,
-    },
-    {
-        id: "Bombillon liso pico alpaca",
-        titulo: "Bombillon liso pico alpaca",
-        imagen: "./fotos/bombillas/bombillonlisopicoalpaca.png",
-        categoria: {
-            nombre: "bombillas",
-            id: "bombillas",
-        },
-        precio: 4000,
-    },
-    {
-        id: "Bombillon alpaca cincelada sin pico",
-        titulo: "Bombillon alpaca cincelada sin pico",
-        imagen: "./fotos/bombillas/bombillonalpacasinpico.jpeg",
-        categoria: {
-            nombre: "bombillas",
-            id: "bombillas",
-        },
-        precio: 4200,
-    },
-
-                    // YERBAS
-    
-    {
-        id: "Yerba canarias",
-        titulo: "Yerba canarias",
-        imagen: "./fotos/yerbas/yerbacanarias.jpg",
-        categoria: {
-            nombre: "yerbas",
-            id: "yerbas",
-        },
-        precio: 1700,
-    },
-    {
-        id: "Yerba rei verde",
-        titulo: "Yerba rei verde",
-        imagen: "./fotos/yerbas/yerbareiverde.jpg",
-        categoria: {
-            nombre: "yerbas",
-            id: "yerbas",
-        },
-        precio: 1500,
-    },
-    {
-        id: "Yerba la pampa",
-        titulo: "Yerba la pampa",
-        imagen: "./fotos/yerbas/yerbapampa.jpg",
-        categoria: {
-            nombre: "yerbas",
-            id: "yerbas",
-        },
-        precio: 1000,
-    },
-
-                    // SET MATERO
-    
-    {
-            id: "Set matero marron",
-            titulo: "Set matero marron",
-            imagen: "./fotos/setmatero/setmatero1.jpg",
-            categoria: {
-                nombre: "setmatero",
-                id: "setmatero",
-            },
-            precio: 3000,
-    },
-    {
-        id: "Set matero rosa",
-        titulo: "Set matero rosa",
-        imagen: "./fotos/setmatero/setmatero2.jpg",
-        categoria: {
-            nombre: "setmatero",
-            id: "setmatero",
-        },
-        precio: 3100,
-    },
-    {
-        id: "Set matero negro",
-        titulo: "Set matero negro",
-        imagen: "./fotos/setmatero/setmatero3.jpg",
-        categoria: {
-            nombre: "setmatero",
-            id: "setmatero",
-        },
-        precio: 3400,
-    },
-    ];
+fetch("./js/stockProductos.json")
+    .then(response => response.json())
+    .then(data => {
+        stockProductos = data;
+        cargarProductos(stockProductos);
+    })
 
 const contenedorProductos = document.querySelector("#contenedor-productos");
 const botonesCategorias = document.querySelectorAll(".boton-categoria");
@@ -207,14 +33,14 @@ function cargarProductos(productosElegidos) {
         `;
 
         contenedorProductos.append(div);
-    
+
     })
 
-    actualizarBotonesAgregar(); 
+    actualizarBotonesAgregar();
 }
 
-cargarProductos(stockProductos);
-    
+
+
 botonesCategorias.forEach(boton => {
     boton.addEventListener("click", (e) => {
 
@@ -227,9 +53,9 @@ botonesCategorias.forEach(boton => {
         } else {
             cargarProductos(stockProductos);
         }
-        
 
-        
+
+
     })
 });
 
@@ -241,9 +67,39 @@ function actualizarBotonesAgregar() {
     });
 }
 
-const productosEnCarrito = [];
+let productosEnCarrito;
+
+let productosEnCarritoLS = localStorage.getItem("productos-en-carrito");
+if (productosEnCarritoLS) {
+    productosEnCarrito = JSON.parse(productosEnCarritoLS);
+    actualizarNumerito();
+} else {
+    productosEnCarrito = [];
+}
+
 
 function agregarAlCarrito(e) {
+
+    Toastify({
+        text: "Producto agregado",
+        duration: 2500,
+        close: false,
+        gravity: "top", 
+        position: "right", 
+        stopOnFocus: true, 
+        style: {
+            background: "linear-gradient(to right, #25d266, #25d266)",
+            borderRadius: "20px",
+            textTransform: "uppercase",
+            fontSize: "15px",
+        },
+        offset: {
+            x: 40, 
+            y: 80,
+        },
+        onClick: function () {} 
+    }).showToast();
+    
 
     const idBoton = e.currentTarget.id;
     const productoAgregado = stockProductos.find(producto => producto.id === idBoton);
@@ -251,18 +107,17 @@ function agregarAlCarrito(e) {
     if (productosEnCarrito.some(producto => producto.id === idBoton)) {
         const index = productosEnCarrito.findIndex(producto => producto.id === idBoton);
         productosEnCarrito[index].cantidad++;
-    }else {
+    } else {
         productoAgregado.cantidad = 1;
         productosEnCarrito.push(productoAgregado);
     }
 
     actualizarNumerito();
-    localStorage.setItem("productos-en-carrito", JSON.stringify(productosEnCarrito)); 
+    localStorage.setItem("productos-en-carrito", JSON.stringify(productosEnCarrito));
 }
 
 function actualizarNumerito() {
     let nuevoNumerito = productosEnCarrito.reduce((acc, producto) => acc + producto.cantidad, 0);
     numerito.innerText = nuevoNumerito;
-    
-}
 
+}
